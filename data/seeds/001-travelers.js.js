d9fab10abfc01552a1bf97f8bfd0e1619a5ce13c
jrizza88 <jetorres88@gmail.com>
2019-04-18 12:35:21 -0400

    set up migrations and seeds

---
+++ data/seeds/001-travelers.js.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('travelers').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('travelers').insert([
+        {id: 1, name: 'Jamar'},
+        {id: 2, name: 'Ryan'},
+        {id: 3, name: 'Juan'},
+        {id: 4, name: 'Sam'},
+      ]);
+    });
+};

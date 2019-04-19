ea7db1de26f1bc8ace27db628482715c00b0be95
jrizza88 <jetorres88@gmail.com>
2019-04-18 20:40:38 -0400

    MVP complete

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 
-// const travelers = require('../travelers/travelersModel');
+const Travelers = require('../travelers/travelersModel');
 
 const server = express();
 
@@ -11,8 +11,28 @@ server.get('/', async (req, res) => {
     res.status(200).json({message: 'Welcome traveler! Your server is running'})
 });
 
-// server.get('/travelers', async(req, res) => {
-//     res.status(200).json()
-// })
+server.get('/travelers', async(req, res) => {
+    const travelers = await Travelers.getAll();
+    res.status(200).json(travelers)
+})
+
+server.post('/travelers', async(req, res) => {
+        try{
+            const traveler = await Travelers.insert(req.body)
+      
+            res.status(201).json({traveler})
+        } catch (err) {
+            res.status(500).json(err)
+        } 
+});
+
+server.delete('/travelers:id', async(req, res) => {
+    try{
+        await Travelers.remove({ id: req.params.id })
+        res.status(202).json({ message: 'traveler left the group :( '})
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -7,7 +7,7 @@ const db = require('../data/dbConfig');
 describe('GET /', () => {
     
     it('should return status 200', async () => {
-        
+    
         const res = await request(server).get('/');
 
         expect(res.status).toBe(200);
@@ -24,4 +24,39 @@ describe('GET /', () => {
 
         expect(res.type).toBe('application/json');
     })
+})
+
+describe('GET /travelers', () => {
+    
+    it('should return status 200 for travelers endpoint', async () => {
+    
+        const res = await request(server).get('/travelers');
+        expect(res.status).toBe(200);
+    });
+
+    it('should return JSON of all listed travelers', async () => {
+        const res = await request(server).get('/travelers')
+
+        expect(res.type).toBe('application/json');
+    })
+})
+
+describe('POST /travelers',async () => {
+    
+    it('should return status 201 for new traveler', async () => {
+        const res = await request(server).post('/travelers')
+            .send({ name: 'Emerson'})
+        expect(res.status).toBe(201);
+    });
+})
+
+describe('DELETE /travelers',async () => {
+    
+    it('should return the right response', async () => {
+    
+        const res = await request(server).delete('/travelers:id');
+
+        expect(res.status).toBe(202);
+    });
+
 })
